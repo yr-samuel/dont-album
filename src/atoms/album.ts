df80d28072af5b0359248a16b55452a99071601a
Samuel <samuel.aoliveira74@gmail.com>
2022-10-17 16:28:16 -0300

    refactor: changed default value of albumState

--- src/atoms/album.ts
+++ src/atoms/album.ts
@@ -1,7 +1,7 @@
 import { atom } from "recoil";
 import { Album } from "../interfaces";
 
-export const albumState = atom<Album | null>({
+export const albumState = atom<Album>({
 	key: "album",
-	default: null,
+	default: [],
 });
