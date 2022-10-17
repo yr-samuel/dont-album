7f9f3c7db2fa3ff585d8995168bc1752a5100fe8
Samuel <samuel.aoliveira74@gmail.com>
2022-10-17 16:17:32 -0300

    refactor: remove index.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./config/firebase";
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { RecoilRoot } from "recoil";
 import "bootstrap/dist/js/bootstrap.bundle.min";
